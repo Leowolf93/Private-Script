779fc731cb909fbf125946dd8bc79f3553304421
Leowolf93 <115647782+Leowolf93@users.noreply.github.com>
2023-08-27 07:48:09 +0800

    Update Wins.js

--- Wins.js
+++ Wins.js
@@ -2,4 +2,5 @@ var body = $response.body;
 var obj = JSON.parse(body);
 obj.response.trial.duration = 99;
 body = JSON.stringify(obj);
+$notify("Surge Notification", "This is a subtitle", "Hello from Surge!");
 $done({body});
