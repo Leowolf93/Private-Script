891b7d97402a1a75847895bd16004f997a1883e0
Leowolf93 <115647782+Leowolf93@users.noreply.github.com>
2023-08-27 07:56:51 +0800

    Update Wins.js

--- Wins.js
+++ Wins.js
@@ -1,8 +1,1 @@
-var body = $response.body;
-var obj = JSON.parse(body);
 console.log("This is a log message.");
-console.log(obj.response.trial.duration);
-obj.response.trial.duration = 99;
-console.log(obj.response.trial.duration);
-body = JSON.stringify(obj);
-$done({body});
