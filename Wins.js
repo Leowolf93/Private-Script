6558b907067d6064e99cc53a7e7fd4f1005899c9
Leowolf93 <115647782+Leowolf93@users.noreply.github.com>
2023-09-01 13:32:55 +0800

    Update Wins.js
    final

--- Wins.js
+++ Wins.js
@@ -1,8 +1,3 @@
 let obj = JSON.parse($response.body);
-obj.response.product_id = "781505";
-obj.response.activation_id = "admin";
-obj.response.type = "personal";
-obj.response.expires = 1;
-obj.response.expiry_date = 1999999999999;
-//obj.response.trial.duration = 9999
+obj.response.trial.duration = 9999
 $done({body: JSON.stringify(obj)});
