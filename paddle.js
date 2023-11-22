8d72a94a8c878ee7b96b4804a8ad23c62dd4ab79
Leowolf93 <115647782+Leowolf93@users.noreply.github.com>
2023-11-22 16:19:38 +0800

    Update paddle.js

--- paddle.js
+++ paddle.js
@@ -1,7 +1,11 @@
 let url = $request.url;
 
-let paddleActivate = () => {
+let charlieActive = () => {
     if (url !== "https://v3.paddleapi.com/3.2/license/activate") return;
+    let userAgent = $request.headers['User-Agent'];
+    if (!userAgent.includes("Downie") || !userAgent.includes("Permute")) {
+        return; // 如果不包含 "Downie" 或 "Permute"，则退出函数
+    }
     let body = $request.body.split("&");
     let product_id = "";
     for (let k of body) {
@@ -30,8 +34,12 @@ let paddleActivate = () => {
     });
 };
 
-let paddleVerify = () => {
+let charlieVerify = () => {
     if (url !== "https://v3.paddleapi.com/3.2/license/verify") return;
+    let userAgent = $request.headers['User-Agent'];
+    if (!userAgent.includes("Downie") || !userAgent.includes("Permute")) {
+        return; // 如果不包含 "Downie" 或 "Permute"，则退出函数
+    }
     let body = $request.body.split("&");
     let product_id = "";
     for (let k of body) {
@@ -56,6 +64,49 @@ let paddleVerify = () => {
         },
     });
 };
+let paddleActivate = () => {
+    if (url !== "https://v3.paddleapi.com/3.2/license/activate") return;
+    let body = $request.body.split("&");
+    let product_id = "";
+    for (let k of body) {
+        if (k.indexOf("product_id") != -1) {
+            product_id = k.split("=")[1];
+        }
+    }
 
+    $done({
+        response: {
+            body: JSON.stringify({
+                success: true,
+                response: {
+                    product_id: product_id,
+                    activation_id: "99567@qq.com",
+                    type: "personal",
+                    expires: 1,
+                    expiry_date: 1999999999999,
+                },
+            }),
+        },
+    });
+};
+
+let paddleVerify = () => {
+    if (url !== "https://v3.paddleapi.com/3.2/license/verify") return;
+    let body = JSON.stringify({
+        success: true,
+        response: {
+            type: "personal",
+            expires: 1,
+            expiry_date: 1999999999999,
+        },
+    });
+    $done({
+        response: {
+            body,
+        },
+    });
+};
+charlieActive();
+charlieVerify();
 paddleActivate();
 paddleVerify();
